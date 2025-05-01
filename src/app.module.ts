e47969ca125f4a3324900cb56e479c3f88aaa38a
Sabina Zaripova <sabina.zaripova@nu.edu.kz>
2025-05-02 00:48:47 +0500

    Add new modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {AuthModule} from "./auth/auth.module";
+import {DatabaseModule} from "./database/database.module";
+import {ConfigModule} from "@nestjs/config";
+import Joi from "@hapi/joi";
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number()
+      })
+  }),
+    DatabaseModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
