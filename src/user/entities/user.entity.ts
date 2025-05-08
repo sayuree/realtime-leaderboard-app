530d98915fe72f6e7fb29835dd7da6b9949bfe7b
Sabina Zaripova <sabina.zaripova@nu.edu.kz>
2025-05-09 00:58:07 +0500

    Add otp to user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,38 +1,63 @@
 import {BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 import {hash} from "bcryptjs";
 import {Score} from "../../score/entities/score.entity";
+import {IsBoolean, IsEmail, IsNotEmpty, IsString} from "class-validator";
 
 @Entity('users')
 export class User {
     @PrimaryGeneratedColumn('uuid')
     public id: string;
 
+    @IsNotEmpty()
+    @IsString()
     @Column({ name: 'first_name' })
     public firstName: string;
 
     @Column({ name: 'last_name' })
     public lastName: string;
 
+    @IsEmail()
+    @IsNotEmpty()
     @Column({ unique: true })
     public email: string;
 
-    @Column()
+    @IsString()
+    @IsNotEmpty()
+    @Column({ select: false })
     public password: string;
 
+    @IsString()
     @Column()
     public username: string;
 
-    @Column()
+    @IsBoolean()
+    @Column({ default: false })
     public is_admin: boolean;
 
-    @OneToMany(() => Score, score => score.user)
+    @OneToMany(() => Score, score => score.user, {
+        cascade: true,
+        onDelete: 'CASCADE'
+    })
     scores: Score[];
 
-    @Column({ name: 'created_at' })
+    @Column({ default: false })
+    verified: boolean;
+
+    @Column({ name: 'otp_code', nullable: true })
+    otpCode: string;
+
+    @Column({ name: 'otp_expires', nullable: true })
+    otpExpires: Date;
+
+    @Column({ name: 'created_at', default: () => 'CURRENT_TIMESTAMP' })
     createdAt: Date;
 
-    @Column({ name: 'updated_at' })
+    @Column({ name: 'updated_at', default: () => 'CURRENT_TIMESTAMP',
+        onUpdate: 'CURRENT_TIMESTAMP' })
     updatedAt: Date;
+
+    @Column({ name: 'deleted_at', nullable: true })
+    deletedAt: Date;
 
     get fullName(): string {
         return `${this.firstName} ${this.lastName}`;
